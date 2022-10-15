45d6adfc3c010094942c247d5c9a787edb347921
Aliny <104106525+NovaisA@users.noreply.github.com>
2022-10-15 13:55:00 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import AppNavigation from "./navigation/AppNavigation";
+import Constants from "./constants";
+import Header from "./components/Header";
 
 export default function App() {
   return <AppNavigation />;
+ 
 }
 
 const styles = StyleSheet.create({
