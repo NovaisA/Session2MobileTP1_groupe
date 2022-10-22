f95c128da83493671a99bf220d60ee95e4eb197e
Aliny <104106525+NovaisA@users.noreply.github.com>
2022-10-22 17:27:30 -0400

    Update Inscription.js

--- screens/Inscription.js
+++ screens/Inscription.js
@@ -1,49 +1,54 @@
-import React, { useState } from "react";
-import { View, TextInput, StyleSheet, TouchableOpacity ,Button} from "react-native";
 
-export default function Inscription() {
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, TouchableOpacity,Text } from "react-native";
+
+
+export default function Register({navigation}) {
     const [nomComplet, setNomComplet] = useState("");
     const [courriel, setCourriel] = useState("");
     const [motDePasse, setMotDePasse] = useState("");
     const [confirmation, setConfirmation] = useState("");
-    const [telephone, setTelephone] = useState([]);
+    const [telephone, setTelephone] = useState("");
 
     return (
         <View style={styles.container}>
             <TextInput
                 style={styles.input}
                 placeholder="Nom Complet"
+                value={nomComplet}
                 onChangeText={(text) => setNomComplet(text)}
             />
-
             <TextInput
-                style={styles.input}
+                style={styles.input }
                 placeholder="Email"
+                value={courriel}
                 onChangeText={(text) => setCourriel(text)}
             />
-
             <TextInput
                 style={styles.input}
                 placeholder="Mot de passe"
+                value={motDePasse}
                 onChangeText={(text) => setMotDePasse(text)}
             />
             <TextInput
                 style={styles.input}
                 placeholder="Confirmation"
+                value={confirmation}
                 onChangeText={(text) => setConfirmation(text)}
             />
-
             <TextInput
                 style={styles.input}
                 placeholder="Cellulaire"
                 keyboardType="numeric"
+                value={telephone}
                 onChangeText={(text) => setTelephone(text)}
             />
-
-            <View style={styles.button}>
-                <Button title="S'enregistrer"/>
-            </View>
-
+           <TouchableOpacity 
+           title="Register"
+           style={styles.touchables}
+           onPress={() => navigation.navigate('Login')}>
+           <Text style={styles.register}>Register</Text>
+           </TouchableOpacity>
         </View>
     )
 }
@@ -61,13 +66,21 @@ const styles = StyleSheet.create({
         borderRadius: 6,
     },
 
-    button: {
+    touchables: {
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: " red",
         padding: 10,
         margin: 12,
-        borderRadius: 6,
+        borderRadius: 10,
+        backgroundColor:"blue",
     },
+
+    register: {
+            fontSize: 20,
+            textAlign: 'center',
+            fontWeight: "bold",
+            color: "#fff",
+    }
+
 });
 
