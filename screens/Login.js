76fc56a62e6859e0e0271f9d96be4a1b0f032470
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-10-23 09:21:04 -0400

    Add files via upload

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,94 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function seConnecter(paramNavigation) {
+    //connect and authenticate into firebase
+    navigation.navigate(paramNavigation);
+  }
+
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <>
+      <View style={styles.logoContainer}>
+        <MaterialCommunityIcons
+          name="human-greeting-proximity"
+          size={150}
+          color="black"
+        />
+      </View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => seConnecter("Liste Contact")}
+        >
+          <Text style={styles.login}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Accueil")}
+        >
+          <Text style={styles.login}>Accueil</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  logoContainer: {
+    backgroundColor: "#F5FCFF",
+    alignItems: "center",
+    paddingBottom: 100,
+    paddingTop: 50,
+  },
+  login: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "white",
+  },
+  input: {
+    height: 50,
+    width: 200,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 18,
+    padding: 10,
+    margin: 10,
+  },
+  button: {
+    width: 150,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 19,
+    marginTop: 20,
+    borderColor: "blue",
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
