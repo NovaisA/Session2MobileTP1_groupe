be2430cbcb92d6b65c8964613685ab8b53a58958
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-10-23 20:01:31 -0400

    Add files via upload

--- database/firebase.js
+++ database/firebase.js
@@ -1,24 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1usS1qUhLjQKEZG-mcZsaYofYdI7qsYs",
-  authDomain: "coursprogmobilelabo01.firebaseapp.com",
-  databaseURL: "https://coursprogmobilelabo01-default-rtdb.firebaseio.com",
-  projectId: "coursprogmobilelabo01",
-  storageBucket: "coursprogmobilelabo01.appspot.com",
-  messagingSenderId: "181378338546",
-  appId: "1:181378338546:web:31dde654b8e0eefce00a6b",
-  measurementId: "G-P2055XDM0B",
+  apiKey: "AIzaSyCfnV5MPy0OVo39gaNL4ZcOUJCofm0MHMs",
+  authDomain: "tpcoursmobile.firebaseapp.com",
+  databaseURL: "https://tpcoursmobile-default-rtdb.firebaseio.com",
+  projectId: "tpcoursmobile",
+  storageBucket: "tpcoursmobile.appspot.com",
+  messagingSenderId: "811097323774",
+  appId: "1:811097323774:web:2f4f9ea7e1a7cb29e14daf",
+  measurementId: "G-ZD92ZWZ1BT",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
 export default app;
